092eaba7488d2b09927dd3adc1beb91a5d000a83
psafflow <psafflow@student.21-school.ru>
2020-05-29 21:50:38 +0300

    number5

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: psafflow <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/09 13:43:34 by psafflow          #+#    #+#             */
-/*   Updated: 2020/05/17 21:43:24 by psafflow         ###   ########.fr       */
+/*   Updated: 2020/05/28 21:43:24 by psafflow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@ int	ft_atoi(const char *nptr)
 {
 	int	number;
 	int	sign;
+	int i;
 
 	number = 0;
 	sign = 1;
+	i = 0;
 	while (*nptr != '\0' && ((*nptr >= 9 && *nptr <= 13) || *nptr == 32))
 		nptr++;
 	if (*nptr == '-')
@@ -25,9 +27,10 @@ int	ft_atoi(const char *nptr)
 		nptr++;
 	while (*nptr != '\0' && (*nptr >= '0' && *nptr <= '9'))
 	{
-		if (number != 0 && 0x7FFFFFFFFFFFFFFF / number < 10)
+		i++;
+		if (number != 0 && i > 10)
 		{
-			if (sign == '-')
+			if (sign == -1)
 				return (0);
 			return (-1);
 		}
--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -23,7 +23,7 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	{
 		while (--size && src[i])
 		{
-			dest[i] = src [i];
+			dest[i] = src[i];
 			i++;
 		}
 		dest[i] = '\0';
